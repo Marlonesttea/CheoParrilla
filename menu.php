4223accdfa7e840b0f6436ed28416843ec54e6ca
eliaspalacios-28 <elias.palacios@ieblanquizal.edu.co>
2026-08-26 16:24:48 -0500

    AvancesMenu

--- menu.php
+++ menu.php
@@ -6,18 +6,141 @@ incluirTemplates('header');
 
 // include 'includes/templates/header.php';
 ?>
+        <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images13.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Chuzo de pollo</h3>
+                    <p>Chuzo de Pollo marinado </p>
+                    <span>19.500</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images14.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Chuzo de cerdo</h3>
+                    <p>Chuzo de cerdo marinado </p>
+                    <span>19.500</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images15.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Carne de cerdo</h3>
+                    <p>Carne de cerdo </p>
+                    <span>20.500</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+             <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images16.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Carne de res</h3>
+                    <p>Carne de res </p>
+                    <span>21.500</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images17.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Chuleta</h3>
+                    <p>Chuleta </p>
+                    <span>21.500</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images18.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Costichic</h3>
+                    <p>Costichic </p>
+                    <span>21.500</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images19.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Pechuga</h3>
+                    <p>Pechuga </p>
+                    <span>21.500</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images20.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Pechuga2</h3>
+                    <p>Pechuga bañada en salsa de champiñones </p>
+                    <span>24.500</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images21.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Carne de res envinada</h3>
+                    <p>Carne de res envinada </p>
+                    <span>24.500</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images22.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Pechuga3</h3>
+                    <p>Pechuga gratinada </p>
+                    <span>25.500</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images23.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Punta de Anca</h3>
+                    <p>Punta de Anca </p>
+                    <span>29.000</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images24.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Churrasco</h3>
+                    <p>Churrasco</p>
+                    <span>29.000</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+            <!-- inicia tarejta -->
+            <div class="card">
+                <img src="imgmenú/images25.png" alt="plato1">
+                <div class="card-body">
+                    <h3>Picada</h3>
+                    <p>Picada para 2 personas </p>
+                    <span>32.000</span>
+                </div>
+            </div>
+            <!-- termina tarjeta -->
+
+
 
 
     
-
-    <section class="menu container" id="menu">
-        <div class="Menú">
-            <h2>Asados y carnes</h2>
-            <div class="asadosCarnes">
-            
-            </div>
-        </div>
-    </section>
 
 
 
