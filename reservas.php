d39cde003dc455b45b839610762bbd88f430f9fa
JulesRomana <juliana.romana@ieblanquizal.edu.co>
2026-09-07 16:00:31 -0500

    avance,reservas

--- reservas.php
+++ reservas.php
@@ -4,9 +4,39 @@ $scripts = ['app', 'contacto']; // global + específico
 require 'includes/funciones.php';
 incluirTemplates('header');
 include 'includes/templates/loader.php';
-
-
 ?>
+<h1 id="apartar">Reservas</h1>
+<p id="descripcion">Reserva tu lugar fácil y rápido.</p>
+<a href="https://wa.me/573234382813?text=Hola,%20quiero%20reservar" class="btn">Reservar Ahora</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
