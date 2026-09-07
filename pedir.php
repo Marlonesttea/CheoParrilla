2fbafd223927fc5d73b11e5d1fe79d335defa218
ZabdiDG <zadiga8@gmail.com>
2026-09-07 16:02:52 -0500

    Adelanto de Pedir Ahora

--- pedir.php
+++ pedir.php
@@ -7,9 +7,15 @@ include 'includes/templates/loader.php';
 
 
 ?>
+<div class="pedir">
+    <h1> ¡Pide ahora!</h1>
+    <p>Pide lo que desees desde la comodidad de tu hogar, vía WhatsApp</p>
+    <div>
+    <a href="https://wa.me/573234382813?text=Hola,%20quiero%20hacer%20un%20pedidos" class="btn">Realizar Pedido</a>
+    </div>
+</div>
 
-
-
-
-
+<body>
+    <img src="img/111453.jpg" alt="imgpedir">
+</body>
 <?php  include 'includes/templates/footer.php' ?>
