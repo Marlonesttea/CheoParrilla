725dac08e086cedd6e044eb59f921ab0c01783fe
Marlonesttea <marlon.sierra@ieblanquizal.edu.co>
2026-09-14 14:23:51 -0500

    Cosos Juanes

--- nosotros.php
+++ nosotros.php
@@ -9,7 +9,28 @@ include 'includes/templates/loader.php';
 ?>
 
 
+<div class="Nosotros container">
+    <h1></h1>
+    <p class="Nosotros-texto">Cheo Parrilla, donde cada bocado tiene su propia historia y el verdadero sabor de la parrilla se convierte en un momento que siempre querrás repetir.”</p>
 
+
+
+</div>
+
+<div>
+<h2></h2>
+<p></p>
+</div>
+
+<div>
+<h2></h2>
+<p></p>
+</div>
+
+<div>
+<h2></h2>
+<p></p>
+</div>
 
 
 <?php  include 'includes/templates/footer.php' ?>
