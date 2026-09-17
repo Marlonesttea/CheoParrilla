b3124ad98f99aecf098d75ae4d33d48b0cb4455c
Marlonesttea <marlon.sierra@ieblanquizal.edu.co>
2026-09-17 00:24:03 -0500

    HeroPedir arreglado  -M

--- pedir.php
+++ pedir.php
@@ -7,16 +7,24 @@ include 'includes/templates/loader.php';
 
 
 ?>
+
+<section class="pedir-hero">
+        <div class="overlay"></div>
+        <div class="pedir-hero-content">
+            <h1 class="cheoTransicion">¡Pide ahora!</h1>
+            <p>Pide lo que desees desde la comodidad de tu hogar, vía WhatsApp</p>
+            <a href="https://wa.me/573234382813?text=Hola,%20quiero%20hacer%20un%20pedidos" class="btn">Realizar Pedido</a>
+        </div>
+    </section>
+    
 <div class="pedir">
     <h1> ¡Pide ahora!</h1>
     <p>Pide lo que desees desde la comodidad de tu hogar, vía WhatsApp</p>
     <div class= "btnpedir">
     <a href="https://wa.me/573234382813?text=Hola,%20quiero%20hacer%20un%20pedidos" class="btn">Realizar Pedido</a>
 
-    <section class="heropedir">
-  <div class="hero-content">
-  </div>    
-</section>
+
+
 
 <style>
     body{
