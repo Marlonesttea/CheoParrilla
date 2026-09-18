53ec61b439635559eed10397d1178fa9bd7564dc
ZabdiDG <zadiga8@gmail.com>
2026-09-18 16:17:56 -0500

    tarjetasPedirAhora -Z

--- pedir.php
+++ pedir.php
@@ -15,30 +15,11 @@ include 'includes/templates/loader.php';
             <p>Pide lo que desees desde la comodidad de tu hogar, vía WhatsApp</p>
             <a href="https://wa.me/573234382813?text=Hola,%20quiero%20hacer%20un%20pedidos" class="btn">Realizar Pedido</a>
         </div>
+        
     </section>
-    
-<div class="pedir">
-    <h1> ¡Pide ahora!</h1>
-    <p>Pide lo que desees desde la comodidad de tu hogar, vía WhatsApp</p>
-    <div class= "btnpedir">
-    <a href="https://wa.me/573234382813?text=Hola,%20quiero%20hacer%20un%20pedidos" class="btn">Realizar Pedido</a>
 
-
-
-
-<style>
-    body{
-        background-image: url('img/114093.png');
-        background-size: cover;
-        background-position: center center;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        filter: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('img/114093.png');
-
-    }
-</style>
-    </div>
-
+<div class="pedir container">
+    <?php  include 'includes/templates/PedirCards.php' ?>
 </div>
 
 
